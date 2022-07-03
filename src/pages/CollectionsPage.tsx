c0e4bdc75ad2e1d8e3f86b4fd492ba8434e6394f
Aerim Yi <aerim.yi@immutable.com>
2022-07-03 15:09:36 +1000

    create collection page layout

---
+++ src/pages/CollectionsPage.tsx
@@ -0,0 +1,20 @@
+import { Fragment } from "react"
+import HeaderBar from "../components/HeaderBar/HeaderBar"
+import { Container, Row, Col } from 'react-bootstrap';
+
+const CollectionsPage = () => {
+    return (
+        <Fragment>
+            <HeaderBar /> 
+            <Container>
+                <Row>
+                    <Col>
+                    {/* This is where the cards will be displayed */}
+                    </Col>
+                </Row>
+            </Container>
+        </Fragment>
+    )
+}
+
+export default CollectionsPage
--- src/pages/ExamplePage.tsx
+++
@@ -1,7 +0,0 @@
-const ExamplePage = () => {
-    return (
-        <div>ExamplePage</div>
-    )
-}
-
-export default ExamplePage
