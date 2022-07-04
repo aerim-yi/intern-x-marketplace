6c1e10921be6f5ee6969be2660ca6a9d6bec9b77
Jinyoung Kim <jinyoung.kim@Jinyoungs-MBP.modem>
2022-07-04 14:48:19 +1000

    card-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import './App.css';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'NFT #1',
+      amount: 94.12,
+    },
+    { id: 'e2', 
+      title: 'NFT #2', 
+      amount: 799.49},
+    {
+      id: 'e3',
+      title: 'NFT #3',
+      amount: 294.67,
+    },
+    {
+      id: 'e4',
+      title: 'NFT #4',
+      amount: 450,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          hello heroku how are you?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Intern NFT Marketplace</h2>
+      <Expenses items={expenses} /> 
+      <Expenses items={expenses} />
     </div>
   );
 }
