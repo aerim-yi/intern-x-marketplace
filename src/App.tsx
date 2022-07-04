d9d636741150602d677f3cf03eb5bf7d9dd6713b
Jinyoung Kim <jinyoung.kim@Jinyoungs-MBP.modem>
2022-07-04 14:48:19 +1000

    card-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
+<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CollectionsPage from './pages/CollectionsPage';
+=======
+import './App.css';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'NFT #1',
+      amount: 94.12,
+    },
+    { id: 'e2', 
+      title: 'NFT #2', 
+      amount: 799.49},
+    {
+      id: 'e3',
+      title: 'NFT #3',
+      amount: 294.67,
+    },
+    {
+      id: 'e4',
+      title: 'NFT #4',
+      amount: 450,
+    },
+  ];
+>>>>>>> 6c1e109 (card-component)
+
   return (
     <div>
+<<<<<<< HEAD
       <CollectionsPage />
+=======
+      <h2>Intern NFT Marketplace</h2>
+      <Expenses items={expenses} /> 
+      <Expenses items={expenses} />
+>>>>>>> 6c1e109 (card-component)
     </div>
   );
 }
