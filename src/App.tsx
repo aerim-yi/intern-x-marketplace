a9a7cac6e13df8ce54eb6709ab5683e039302f13
Aerim Yi <aerim.yi@immutable.com>
2022-07-06 15:13:18 +1000

    feat: add routes to the collection cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CollectionsPage from './pages/CollectionsPage';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import OrderPage from './pages/OrderPage';
 
 function App() {
   return (
-    <div>
-      <CollectionsPage />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <CollectionsPage />
+        </Route>
+        <Route path="/collection/:collectionId">
+          <OrderPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,44 +1,26 @@
-import { useState, useEffect } from "react";
 import HeaderBar from "../components/HeaderBar/HeaderBar"
 import { useParams } from "react-router-dom"
-import { getOrders } from "../api/orders-api";
 import { Container, Row, Col } from "react-bootstrap";
-import { Order } from '@imtbl/core-sdk'
+
+interface Params {
+    collectionId: string
+}
 
 const OrderPage = () => {
-    const params = useParams();
-    const [result, setResult] = useState<Order[]>([])
-
-    // inspired by this Stackflow answer:https://stackoverflow.com/questions/53332321/react-hook-warnings-for-async-function-in-useeffect-useeffect-function-must-ret
-    // useEffect(() => {
-    //     async function fetchCollection() {
-    //         let response = await getOrders(params)
-    //         console.log('response', response)
-    //         setResult(response.result)
-    //     }
-    //     fetchCollection()
-    // }, [])
+    const params = useParams<Params>();
+    console.log(params.collectionId)
+    //  Will use collectionId to call orders API
+    
     return (
         <>
             <HeaderBar />
             <Container>
                 <Row>
-                    {result.length > 0 && result.map((item: Order) => {
-                        <div>
-                            <h1>I am the NFT sale item page</h1>
-                        </div>
-                        return (
-                            <Col xs={12} md={4}>
-                                {/* item cards will be displayed here */}
-                            </Col>
-                        )
-                    })}
-                    {result.length === 0 && <h1> loading.... </h1>}
+                    <Col>
+                        <h1> this is the NFT item page!</h1>
+                    </Col>
                 </Row>
             </Container>
-
-
-
         </>
     )
 }
