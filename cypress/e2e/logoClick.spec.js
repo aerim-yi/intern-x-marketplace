dc4a2566ea2a67981a15ebc55c5e256782ad6aeb
Jinyoung Kim <jinyoung.kim@Jinyoungs-MacBook-Pro.local>
2022-07-21 13:41:52 +1000

    II-96-logo-cypress-test

---
+++ cypress/e2e/logoClick.spec.js
@@ -0,0 +1,17 @@
+// Click logo should return to collection page 
+
+describe('Clicking into a collection ', () => {
+    it('finds the content "type"', () => {
+
+        const testString = "Siwonchoi"
+
+        cy.visit("http://localhost:3000");
+    
+        cy.contains(testString).click();
+        cy.contains('Intern X Marketplace').click();
+        
+        cy.url().should('equal', 'http://localhost:3000/')
+
+      })
+    })
+    
