31d1771df3871dbdd6fe6148268cfb0e7c45d7f4
aerim-yi <aerim.yi@immutable.com>
2022-07-22 10:00:03 +1000

    test: add e2e test for wallet button

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   e2e: {
     baseUrl: 'https://intern-x-marketplace.herokuapp.com',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
