6fecb0720e60d223c11b3868bee891bf762f47a3
aerim-yi <aerim.yi@immutable.com>
2022-08-04 19:06:50 +1000

    ops: Add Sentry monitoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+
+Sentry.init({
+  dsn: "https://d743e4e355714473baabdb7fb5ac3e14@o1344629.ingest.sentry.io/6620469",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
