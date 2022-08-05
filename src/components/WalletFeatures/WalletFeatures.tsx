da9950e4f1fee5f206bd8411eefeae253824043c
Jinyoung Kim <jinyoungkim927@gmail.com>
2022-08-05 13:14:38 +1000

    fix: delete css file and unnecessary imports

--- src/components/WalletFeatures/WalletFeatures.tsx
+++ src/components/WalletFeatures/WalletFeatures.tsx
@@ -1,16 +1,15 @@
-// import "./WalletFeatures.css"
-import { Link, ImmutableXClient, ImmutableMethodResults, ERC721TokenType, ETHTokenType, ImmutableRollupStatus  } from '@imtbl/imx-sdk';
-import { useEffect, useState } from 'react';
+import { Link, ImmutableXClient } from '@imtbl/imx-sdk';
+import { useState } from 'react';
 import DepositFunction from "./DepositFunction";
 import WithdrawFunction from "./WithdrawFunction";
-// import BalanceFunction from "./BalanceFunction";
+
 
 const WalletFeatures = () => {
   const [wallet, setWallet] = useState('undefined');
   const link = new Link('https://link.ropsten.x.immutable.com')
   const [client, setClient] = useState<ImmutableXClient>(Object);
   return (
-    <nav className="WalletFeatures-container">
+    <nav>
       <div>
         <DepositFunction />
       </div>
